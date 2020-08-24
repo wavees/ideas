9d7db8cb2519d59b450e19cf654cb2dde4d026d9
SniperFox213 <dj.gleb@list.ru>
2020-08-24 15:54:48 +0300

    fix: Fixed Socket API url

--- src/network/socket.js
+++ src/network/socket.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import api from "../config/application/api";
 
 // Let's now run our socket
-const socket = io("http://localhost:8080/");
+const socket = io("https://api.ideas.wavees.ml/");
 
 // And now let's just export our
 // socket instance.
